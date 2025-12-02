49dff978020795250baedbd03514837802e98d94
Arslan <91959072+rasulow@users.noreply.github.com>
2025-12-02 13:49:28 +0500

    project start

---
+++ portfolio/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    """
+    Class-Based View for rendering the main portfolio page
+    """
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # You can add any additional context data here
+        context['page_title'] = 'Aydyn Sahra - Portfolio'
+        return context
+
